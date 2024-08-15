4504f69de2cda573e8549da791176a4ae79f1507
kingsley Ibrahim <kingsleynkau@gmail.com>
2024-08-15 19:44:49 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-""" gets the number of subs for a subreddit """
-import json
-import requests
+import sys
 
-
-def number_of_subscribers(subreddit):
-    """ a function that takes a subbreddit and returns the number of subs"""
-        user_agent = {"User-Agent": "unix:0-subs.py:v1.0"}
-            data = requests.get("https://www.reddit.com/r/{}/about.json"
-                    .format(subreddit),
-                    headers=user_agent,
-                    allow_redirects=False)
-            if data.status_code != 200:
-                return 0
-            json_data = data.json()
-            results = json_data.get("data").get("subscribers")
-            return results
-
+if __name__ == '__main__':
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
